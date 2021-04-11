4a6117ac83a00b0ead6ead7d38f139114feb00a2
Evgeniy Kozhushko <d320@mail.ru>
2021-04-11 11:15:57 +0300

    the home work2

---
+++ HomeWork2.py
@@ -0,0 +1,27 @@
+w = float(input("Введите вес, кг "))
+h = float(input("Введите рост, см "))
+
+bmi = w / ((h/100)**2)
+bmi = round(bmi, 1)
+bmi_int = int(round(bmi,0))
+print('Ваш индекс массы тела: ' + str(bmi))
+
+if bmi <= 16:
+    print ('Выраженный дефицит массы тела')
+elif bmi > 16 and bmi <= 18.5:
+    print ('Недостаточная масса тела (дефицит)')
+elif bmi > 18.5 and bmi <= 24:
+    print ('Нормальная масса тела')
+elif bmi > 24 and bmi <= 30:
+    print ('Избыточная масса тела (предожирение)')
+elif bmi > 30 and bmi <= 35:
+    print ('Ожирение I степени')
+elif bmi > 35 and bmi <= 40:
+    print ('Ожирение II степени')
+elif bmi > 40:
+    print ('Ожирение III степени')   
+
+
+a = '='*(bmi_int - 20)
+b = '='*(60 - bmi_int)
+print('20' + str(a) + '|' + str(b)  + '60')
