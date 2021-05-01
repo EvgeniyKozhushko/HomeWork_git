3dfee34b8f770b0df64491a9e7ef52e4d4eade7c
Evgeniy Kozhushko <d320@mail.ru>
2021-05-01 08:19:35 +0300

    the home work6 gen

--- HomeWork6.py
+++ HomeWork6.py
@@ -127,6 +127,18 @@ dict = {
 
     }
 
+def seporator():
+    var = 0
+    while True:
+        if var % 2:
+            var += 1
+            yield [w, w, w, w, b, b, w, w, w, w]
+        else:
+            var += 1
+            yield [w, w, w, w, w, w, w, w, w, w]
+
+seporator_1 = seporator()
+
 def main():
     while True:
         os.system('cls')
@@ -137,26 +149,10 @@ def main():
         min_2 = t1[4]
         s_1 = t1[6]
         s_2 = t1[7]
-
-        sep = w
-
-        if int(s_2) % 2 == 0:
-            sep = b
-            print(w)
-        else:
-            print(sep)
-
-        print(f'{dict.get(h_1)[0]} {dict.get(h_2)[0]}   {dict.get(min_1)[0]} {dict.get(min_2)[0]}   {dict.get(s_1)[0]} {dict.get(s_2)[0]}')
-        print(f'{dict.get(h_1)[1]} {dict.get(h_2)[1]}   {dict.get(min_1)[1]} {dict.get(min_2)[1]}   {dict.get(s_1)[1]} {dict.get(s_2)[1]}')
-        print(f'{dict.get(h_1)[2]} {dict.get(h_2)[2]}   {dict.get(min_1)[2]} {dict.get(min_2)[2]}   {dict.get(s_1)[2]} {dict.get(s_2)[2]}')
-        print(f'{dict.get(h_1)[3]} {dict.get(h_2)[3]}   {dict.get(min_1)[3]} {dict.get(min_2)[3]}   {dict.get(s_1)[3]} {dict.get(s_2)[3]}')
-        print(f'{dict.get(h_1)[4]} {dict.get(h_2)[4]} {sep} {dict.get(min_1)[4]} {dict.get(min_2)[4]} {sep} {dict.get(s_1)[4]} {dict.get(s_2)[4]}')
-        print(f'{dict.get(h_1)[5]} {dict.get(h_2)[5]} {sep} {dict.get(min_1)[5]} {dict.get(min_2)[5]} {sep} {dict.get(s_1)[5]} {dict.get(s_2)[5]}')
-        print(f'{dict.get(h_1)[6]} {dict.get(h_2)[6]}   {dict.get(min_1)[6]} {dict.get(min_2)[6]}   {dict.get(s_1)[6]} {dict.get(s_2)[6]}')
-        print(f'{dict.get(h_1)[7]} {dict.get(h_2)[7]}   {dict.get(min_1)[7]} {dict.get(min_2)[7]}   {dict.get(s_1)[7]} {dict.get(s_2)[7]}')
-        print(f'{dict.get(h_1)[8]} {dict.get(h_2)[8]}   {dict.get(min_1)[8]} {dict.get(min_2)[8]}   {dict.get(s_1)[8]} {dict.get(s_2)[8]}')
-        print(f'{dict.get(h_1)[9]} {dict.get(h_2)[9]}   {dict.get(min_1)[9]} {dict.get(min_2)[9]}   {dict.get(s_1)[9]} {dict.get(s_2)[9]}') 
-
-        time.sleep(1)
+        sep = next(seporator_1)
         
-main()     
+        for i in range(10):
+            print(f'{dict.get(h_1)[i]} {dict.get(h_2)[i]} {sep[i]} {dict.get(min_1)[i]} {dict.get(min_2)[i]} {sep[i]} {dict.get(s_1)[i]} {dict.get(s_2)[i]}')
+        
+        time.sleep(0.5)
+main()
